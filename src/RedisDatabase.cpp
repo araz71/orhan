f1e9bc813c180fa6b61ffc4806a4767752fd4ebb
Amin Aghakhani <amin.aghakhani71@gmail.com>
2023-06-09 17:58:15 +0000

    RedisReply under implementation.

--- src/RedisDatabase.cpp
+++ src/RedisDatabase.cpp
@@ -1,39 +1,12 @@
 #include "RedisDatabase.h"
 
 #include <string>
+#include <optional>
 #include <exception>
 #include <iostream>
 
 using namespace std;
 using namespace orhan;
-
-void RedisDatabase::add_field(const uint32_t device_id, const std::string& field_name,
-		const std::string& value)
-{
-	void* reply = redisCommand("hset %d %s %b", device_id, field_name.c_str(), value.c_str(), value.length());
-	if (reply == NULL) {
-		throw runtime_error("Can not add field to database.");
-	}
-
-	freeReplyObject(reply);
-}
-
-void RedisDatabase::remove_field(const uint32_t device_id, const std::string& field_name) {
-	void* reply = redisCommand("hdel %d %s", device_id, field_name.c_str());
-	if (reply == NULL) {
-		throw runtime_error("Can not remove field from database");
-	{
-}
-
-optional<string> RedisDatabase::get_field(const uint32_t device_id, const std::string& field_name) {
-	void *reply = redisCommand("hget %d %s", device_id, field_name.c_str());
-	if (reply == NULL) {
-		throw runtime_error("Can not retrieve field from database");
-}
-
-vector<string, string> RedisDatabase::get_fields(const uint32_t device_id) {
-
-}
 
 RedisDatabase::RedisDatabase() {
 	redis_context = redisConnect("127.0.0.1", 6379);
@@ -41,12 +14,78 @@ RedisDatabase::RedisDatabase() {
 		throw runtime_error("Can not connect to the redis");
 	}
 
+	add_field(1, "f1", "v1");
+	add_field(1, "f2", "v2");
+	add_field(1, "f3", "v3");
+
+	remove_field(1, "f3");
 	// Select database here
 }
 
 RedisDatabase& RedisDatabase::get_instance() {
 	static RedisDatabase db{};
 	return db;
+}
+
+void RedisDatabase::add_field(const uint32_t device_id, const std::string& field_name,
+		const std::string& value)
+{
+	RedisReply reply(redisCommand(redis_context, "hset %d %s %b", device_id, field_name.c_str(), value.c_str(), value.length()));
+	if (reply == NULL) {
+		throw runtime_error("Can not add field to database.");
+	}
+
+	if (reply->type != REDIS_REPLY_STRING) {
+		throw runtime_error("Error at redis reply for add_field");
+	}
+}
+
+void RedisDatabase::remove_field(const uint32_t device_id, const std::string& field_name) {
+	RedisReply reply = redisCommand(redis_context, "hdel %d %s", device_id, field_name.c_str());
+	if (reply == NULL) {
+		throw runtime_error("Can not remove field from database");
+	}
+
+	if (reply->type != REDIS_REPLY_INTEGER) {
+		throw runtime_error("Error at redis reply type for remove_field");
+	}
+}
+
+optional<string> RedisDatabase::get_field(const uint32_t device_id, const std::string& field_name) {
+	RedisReply reply = redisCommand(redis_context, "hget %d %s", device_id, field_name.c_str());
+	if (reply == NULL) {
+		throw runtime_error("Can not retrieve field from database");
+	}
+
+	if (reply->type != REDIS_REPLY_NIL) {
+		return std::nullopt;
+	}
+
+	if (reply->type != REDIS_REPLY_STRING) {
+		throw runtime_error("Error at redis reply type for get_field");
+	}
+
+	return make_optional(string(reply->str, reply->len));
+}
+
+optional<vector<RedisDatabase::FieldValuePair>> RedisDatabase::get_fields(const uint32_t device_id) {
+	RedisReply reply = redisCommand(redis_context, "hgetall %d", device_id);
+	if (reply == NULL) {
+		throw runtime_error("Can not retrieve fields from database");
+	}
+
+	if (reply->type == REDIS_REPLY_NIL) {
+		return std::nullopt;
+	}
+
+	if (reply->type != REDIS_REPLY_ARRAY) {
+		throw runtime_error("Error at redis reply type for get_fields");
+	}
+
+	for (size_t element_counter = 0; element_counter < reply->elements; element_counter++) {
+		auto element = reply->element[element_counter];
+		cout << string(element->str, element->len) << endl;
+	}
 }
 
 bool RedisDatabase::add_device(const uint32_t device_id, const DeviceInformation& device_inf) {
--- src/RedisDatabase.h
+++ src/RedisDatabase.h
@@ -7,15 +7,22 @@
 #include <hiredis/hiredis.h>
 
 #include <optional>
+#include <string>
+#include <vector>
+#include <utility>
 
 namespace orhan {
 
+class RedisReply;
+
 class RedisDatabase : public DatabaseInterface {
 public:
+	using FieldValuePair = std::pair<std::string, std::string>;
+
 	RedisDatabase(const RedisDatabase&) = delete;
 	RedisDatabase(const RedisDatabase&&) = delete;
 	void operator=(const RedisDatabase&) = delete;
-	
+
 	bool add_device(const uint32_t device_id, const DeviceInformation& device_inf) override;
 	bool add_register(const uint32_t device_id, const RegisterID register_id,
 			const RegisterTypes type, const RegisterAccess access) override;
@@ -35,13 +42,51 @@ protected:
 	// Adds or changes field to/of device
 	void add_field(const uint32_t device_id, const std::string& field_name,
 		const std::string& value);
-	
+
 	void remove_field(const uint32_t device_id, const std::string& field_name);
 
 	// Retrieves field value from given device
 	std::optional<std::string> get_field(const uint32_t device_id, const std::string& field_name);
 
-	
+	std::optional<std::vector<FieldValuePair>> get_fields(const uint32_t device_id);
 };
+
+class RedisReply {
+public:
+	explicit RedisReply(void* reply)
+	: m_reply(reinterpret_cast<redisReply*>(reply))
+	{
+	}
+
+	RedisReply(const void* reply) {
+		m_reply = reinterpret_cast<redisReply*>(const_cast<void*>(reply));
+	}
+
+	RedisReply()
+	: m_reply(NULL)
+	{
+	}
+
+	redisReply* operator->() {
+		return m_reply;
+	}
+
+	bool operator==(void* arg) {
+		if (arg == m_reply) {
+			return true;
+		}
+		return false;
+	}
+
+	~RedisReply() {
+		if (m_reply != NULL) {
+			freeReplyObject(m_reply);
+		}
+	}
+
+private:
+	redisReply* m_reply;
+};
+
 }
 #endif
